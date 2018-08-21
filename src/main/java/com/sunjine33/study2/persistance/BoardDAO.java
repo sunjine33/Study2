e582f4b3ae26851dd9a4b5531295a7a59c671ad2
sunjine33 <sunjine33@naver.com>
2018-08-21 11:45:23 +0900

    remove all modifier in persistance

--- src/main/java/com/sunjine33/study2/persistance/BoardDAO.java
+++ src/main/java/com/sunjine33/study2/persistance/BoardDAO.java
@@ -6,10 +6,10 @@ import com.sunjine33.study2.domain.BoardVO;
 
 public interface BoardDAO {
 	
-	public void create(BoardVO boardvo)throws Exception;
-	public BoardVO read(Integer bno)throws Exception;
-	public void update(BoardVO boardvo)throws Exception;
-	public void delete(Integer bno)throws Exception;
-	public List<BoardVO> listAll()throws Exception;
-	public Integer getMaxbno()throws Exception;
+	 void create(BoardVO boardvo)throws Exception;
+	 BoardVO read(Integer bno)throws Exception;
+	 void update(BoardVO boardvo)throws Exception;
+	 void delete(Integer bno)throws Exception;
+	 List<BoardVO> listAll()throws Exception;
+	 Integer getMaxbno()throws Exception;
 }
