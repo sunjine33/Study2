7ee1242277326ee9d93f3584b6cf895f53991a02
sunjine33 <sunjine33@naver.com>
2018-08-21 11:47:26 +0900

    remove all modifier in BoardService

--- src/main/java/com/sunjine33/study2/service/BoardService.java
+++ src/main/java/com/sunjine33/study2/service/BoardService.java
@@ -5,14 +5,14 @@ import java.util.List;
 import com.sunjine33.study2.domain.BoardVO;
 
 public interface BoardService {
-		public void regist(BoardVO boardvo) throws Exception;
+		void regist(BoardVO boardvo) throws Exception;
 		
-		public BoardVO read(Integer bno) throws Exception;
+		BoardVO read(Integer bno) throws Exception;
 		
-		public void modify(BoardVO boardvo) throws Exception;
+		void modify(BoardVO boardvo) throws Exception;
 		
-		public void remove(Integer bno) throws Exception;
+		void remove(Integer bno) throws Exception;
 		
-		public List<BoardVO> listall() throws Exception;
+		List<BoardVO> listall() throws Exception;
 		
 }
